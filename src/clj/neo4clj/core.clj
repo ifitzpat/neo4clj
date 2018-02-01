6f52306d131cb15625fd96a8d7f0cc5856cc30fe
Ian FitzPatrick <ian@ianfitzpatrick.eu>
2018-02-01 14:40:57 +0100

    removed EmbeddedGraphDatabase [deprecated] in favour of GraphDatabaseFactory

--- src/clj/neo4clj/core.clj
+++ src/clj/neo4clj/core.clj
@@ -8,8 +8,7 @@
                               PropertyContainer
                               NotFoundException
                               TransactionFailureException)
-	   (org.neo4j.kernel EmbeddedGraphDatabase
-                       Traversal)
+     (org.neo4j.graphdb.factory GraphDatabaseFactory)
      (org.neo4j.graphdb.traversal TraversalDescription
                                   Traverser
                                   TraversalBranch
@@ -19,7 +18,7 @@
                               TransactionData)
      (org.neo4j.index.lucene LuceneFulltextIndexService)
      (org.neo4j.helpers Predicate)))
-     
+
 ; Protocols
 
 (defprotocol PElement
@@ -37,18 +36,18 @@
 (defprotocol PropertyWrap
   (setProperties! [this properties])
   (getProperties [this]))
-  
+
 ; Types
 
-(deftype Neo-Node [^Node element])     
+(deftype Neo-Node [^Node element])
 (deftype Neo-Relationship [^Relationship element])
 (deftype Neo-RelationshipType [n]
   RelationshipType
     (name [_] (name n)))
-     
+
 ; Neo Database
-        
-(def ^EmbeddedGraphDatabase *neo* nil)
+
+(def ^GraphDatabaseFactory *neo* nil)
 (def ^LuceneFulltextIndexService *lucene* nil)
 (def ^Transaction *tx* nil)
 
@@ -62,10 +61,10 @@
   "Start a neo4j instance from the given database path, and bind to *neo*"
   [path]
   (do
-    (alter-var-root #'*neo* (fn [_] (EmbeddedGraphDatabase. path)))
+    (alter-var-root #'*neo* (fn [_] (GraphDatabaseFactory. path)))
     (alter-var-root #'*lucene* (fn [_] (LuceneFulltextIndexService. *neo*)))
     (attach-index-handler)))
-  
+
 (defn stop
   "Stop the running neo4j instance and unbind *neo*"
   []
@@ -74,7 +73,7 @@
     (.shutdown *lucene*)
     (alter-var-root #'*neo* (fn [g] nil))
     (alter-var-root #'*lucene* (fn [g] nil))))
-                             
+
 (defmacro with-neo
   "Wrap body inside a neo-start, neo-stop
   Useful for test harness."
@@ -85,7 +84,7 @@
       (do ~@body)
       (finally
         (stop)))))
-     
+
 ; Transactions
 
 (defn success []
@@ -94,7 +93,7 @@
   (.failure *tx*))
 (defn finish []
   (.finish *tx*))
-  
+
 (defmacro do-tx
   "Wraps the contents inside a transaction and implicit do."
   [& body]
@@ -106,12 +105,12 @@
         val#)
       (finally
         (finish))))))
-     
+
 ; PropertyContainer extensions.
-     
+
 (defn boolean? [o]
   (= java.lang.Boolean (type o)))
-     
+
 (defn- object-storage-type
   "Returns best object type for storage of given object, or nil if invalid."
   [o]
@@ -120,7 +119,7 @@
     (string? o)  java.lang.String
     (integer? o) java.lang.Long
     (number? o)  java.lang.Double))
-    
+
 (defn- type-convert
   "Returns a type conversion function for the given Java type."
   [type]
@@ -129,7 +128,7 @@
     (= type java.lang.String)  str
     (= type java.lang.Long)    long
     (= type java.lang.Double)  double))
-     
+
 (defn array-storage-type
   "Accepts a sequence and returns a Java type for that sequence, or otherwise
   throws an exception if a valid type cannot be found."
@@ -147,13 +146,13 @@
           :default (throw (Exception. "Invalid type mix.")))))
     nil
     arr))
-     
+
 (defn- seq-to-array
   "Converts a sequence into a Java array of an appropriate type for storage."
   [sequence]
   (let [arr-type (array-storage-type sequence)]
     (into-array arr-type (map (type-convert arr-type) sequence))))
-     
+
 (extend-type PropertyContainer
   PropertyWrap
   (setProperties! [this properties]
@@ -172,20 +171,20 @@
                 [(keyword k) prop]
                 [(keyword k) (vec prop)])))
           ks)))))
-     
+
 ; Traversals
 
 (def both Direction/BOTH)
 (def incoming Direction/INCOMING)
 (def outgoing Direction/OUTGOING)
-  
+
 (defn predicate
   "Returns a Predicate wrapping of f for use with TraversalDescription.filter"
   [f]
   (reify
     Predicate
       (accept [_ item] (f item))))
-      
+
 (defn pruner
   "Returns a PruneEvaluator wrapping of f for use with
   TraversalDescription.prune"
@@ -193,37 +192,37 @@
   (reify
     PruneEvaluator
       (^boolean pruneAfter [_ ^Path path] (f path))))
-  
+
 (defn where
   "Adds a filter function f onto the traversal and returns the new traversal.
   Only one filter may be used, and a previous filter will be replaced."
   [f ^TraversalDescription traversal]
   (.filter traversal (predicate f)))
-  
+
 (defn prune
   "Adds a prune function f onto the traversal and returns the new traversal.
   The prune filter should return true when traversal should stop. Note that
   the traversal *will* return the node at which the traversal was stopped."
   [f ^TraversalDescription traversal]
   (.prune traversal (pruner f)))
-  
+
 (defn all-but-start
   "Preset call for where, which filters out the start node from a traversals
   results."
   [^TraversalDescription traversal]
   (where #(not= (.startNode %) (.endNode %)) traversal))
-  
+
 (defn get-nodes-from
   "Executes a given traversal and returns the nodes as a result."
   [^Neo-Node node ^TraversalDescription traversal]
   (map #(Neo-Node. %) (.nodes (.traverse traversal (.element node)))))
-  
+
 (defn along-mult [traversal & relations]
   (reduce (fn [^TraversalDescription traversal [type direction]]
     (.relationships traversal (Neo-RelationshipType. type) direction))
     traversal
     (partition 2 relations)))
-    
+
 (defn along
   "Adds valid relationships to be followed in the traversal. Is in addition to
   any previous valid relationships added. If none are set, then all
@@ -248,13 +247,13 @@
   "Returns a new traversal."
   []
   (Traversal/description))
-    
+
 (def single-level-traverse
   (->> (new-traversal)
        (depth-first)
        (max-depth 1)
        (all-but-start)))
-      
+
 (defn related-via-label
   "Returns the neighbour nodes of a node along a given relationship type and
   direction."
@@ -262,7 +261,7 @@
   (->> single-level-traverse
        (along type direction)
        (get-nodes-from node)))
-    
+
 ; Node
 
 (deftype Neo-Node [^Node element]
@@ -316,9 +315,9 @@
   PRelationship
     (start-node [_] (Neo-Node. (.getStartNode element)))
     (end-node [_] (Neo-Node. (.getEndNode element))))
-    
+
 ; Events/Indexing
-    
+
 (defn- deleted-node-classes [deleted-nodes removed-properties]
   (let [removed-nodes (into {} (map (fn [^Node node] [node {}]) deleted-nodes))]
     (reduce
@@ -338,7 +337,7 @@
   (.index *lucene* entity key value))
 (defn- remove-index [entity key]
   (.removeIndex *lucene* entity key))
-  
+
 (defn- class-index? [class key]
   (and class (contains? (*classes* class) key)))
 
@@ -373,7 +372,7 @@
 
 (defn register-indices
   "Adds indices to the defined indices to keep.
-  
+
   Example:
     (register-indices :index :names)"
   [& indices]
@@ -381,7 +380,7 @@
 
 (defn register-classes
   "Registers body as classes and indices.
-  
+
   Example:
     (register-classes
       :Class [:index :names]
@@ -394,13 +393,13 @@
         (into #{} (map name indices))))
         %
         (partition 2 body))))
-    
+
 ; Named Relationships
 
 (defn register-relations
   "Register named relations. Should be in format [:outgoing :incoming] or
   [:both] for names.
-  
+
   Example:
     (register-relations
       [:outgoing :incoming]
@@ -418,7 +417,7 @@
             relations))
         named-relations
         relations))))
-    
+
 ; Search
 
 (defn find-nodes
@@ -430,9 +429,9 @@
       '()))
   ([class index search]
     (find-nodes (str (name class) "__" (name index)) search)))
-    
+
 ; Constructor
-    
+
 (defn ^Neo-Node node!
   "Create a new node with the given map of properties, or blank if none
   provided. If a class is provided, then this is added to the properties as a
@@ -445,7 +444,7 @@
       (setProperties!
         (.createNode *neo*)
         properties)))))
-        
+
 (defn delete!
   "Deletes elements arguments which are Nodes or Relationships."
   [& elements]
